626532852048666ba492b00c9118a431e2e917e4
d-nnis <flashgo@gmx.net>
2012-04-09 04:12:13 +0200

    gib sheets aus confirm_numcount sheet wählen funktioniert nicht, obwohl Wert auch Scalar ist ...
    Signed-off-by: d-nnis <flashgo@gmx.net>

--- excelplay.pl
+++ excelplay.pl
@@ -1,6 +1,7 @@
 use strict;
 use warnings;
-use excel_com;
+use excel_com2;
+use feature qw/say/;
 
 #my $excelfile = "f:\\tee.xlsx";
 #my $excelobj=Excelobject->new();
@@ -15,16 +16,15 @@ use excel_com;
 
 
 my $excelfile = "f:\\poly\\HU-tp4\\2012-03-15 Vereinsliste aktive.xlsx";
-my $excelobj = Excelobject->new() ;
-$excelobj->init_newExcelfile($excelfile,4);
-my $colstart = 2;
+my $excelobj = Excelobject->new();
+#$excelobj->init($excelfile,4);
+$excelobj->init();
+my $colstart = 1;
 my $srow = 23;
 my $scol = 1;
 my @vals;
 while (my $vals_ref = [ $excelobj->readcol($colstart, 2) ] ) {
-	$|=1;
-	print $colstart.",";
-	$|=1;
+	say $colstart.",";
 	my @vals = @{$vals_ref};
 	last if (scalar @vals) == 0;
 	foreach my $val (@vals) {
